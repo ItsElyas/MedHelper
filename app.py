a3eacf5f58ba6b9a76bd881d893cef5038e8220e
Elyas Khoubach <144924676+ItsElyas@users.noreply.github.com>
2025-05-20 14:58:34 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+# backend/app.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)  # creates a Flask app
+
+@app.route("/")         # this sets the route
+def home():             # when someone visits "/", run this
+    return "Welcome to the MedTracker Backend!"
+
+
+@app.route("/ping")
+def ping():
+    return jsonify({"message": "pong"})
+
+@app.route("/status")
+def status():
+    return jsonify({
+        "medicine_taken": False,
+        "time": "10:30 AM"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
