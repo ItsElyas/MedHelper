a6b08a619136e2f8bc32d38bfdc773db0b66e628
ItsElyas <elyaskh04@gmail.com>
2025-05-29 23:33:44 -0400

    Will make an about when I make the navbar

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)  # creates a Flask app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///medtracker.db'  # SQLite database
 db = SQLAlchemy(app)  # initializes the database
@@ -11,12 +12,12 @@ class Medicine(db.Model):
      id = db.Column(db.Integer, primary_key=True)  
      name = db.Column(db.String(150), nullable=False)
      date_created = db.Column(db.DateTime, default = datetime.utcnow)  
-    
-def __repr__(self):
-    return f'<Medicine {self.name}>'
+   
+     def __repr__(self):
+        return f'<Medicine {self.name}>'
 
 @app.route("/")         # this sets the route
-def home():             # when someone visits "/", run this
+def route():             # when someone visits "/", run this
     return "Welcome to the MedTracker Backend!"
 
 @app.route("/homepage")
@@ -27,9 +28,9 @@ def homepage():
 def medicine():
     return render_template("medicine.html")
 
-@app.route("/index")
-def index():
-    return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route("/ping")
 def ping():
@@ -41,6 +42,7 @@ def status():
         "medicine_taken": False,
         "time": "10:30 AM"
     })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
