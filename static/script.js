756f28307a0d946c45fa6762f7d1aae0fd95b7fa
ItsElyas <elyaskh04@gmail.com>
2025-06-01 17:48:45 -0400

    Changed up visuals to actually look nice and started on add medicine with JS

---
+++ static/script.js
@@ -0,0 +1,6 @@
+function showForm() {
+    const form = document.getElementById("MedicineDiv");
+    form.style.display = "block";
+    document.getElementById("addMedicine").style.display = "none";
+    
+}
