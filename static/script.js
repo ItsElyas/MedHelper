1a991d8bfa341e30bcef3d0ce7a2266cd82d185b
ItsElyas <elyaskh04@gmail.com>
2025-06-05 23:10:48 -0400

    started to work on new med div will do alot more tmrw

--- static/script.js
+++ static/script.js
@@ -4,3 +4,21 @@ function showForm() {
     document.getElementById("addMedicine").style.display = "none";
     
 }
+//counter to tell user how many medications they are taken
+let medCount = 0;
+
+function submit() {
+    // Gets all the values of the inputs
+    const name = document.getElementById("medicineName").value;
+    const dose = document.getElementById("medicineDose").value;
+    const frequency = document.getElementById("Frequency").value;
+    const time = document.getElementById("timeToTake").value;
+
+    // Makes sure the user fills everything out
+    if(!name || !dose || !frequency || !time) {
+        alert("Please fill out all fields.");
+        return
+    }
+
+    medCount++;
+}
