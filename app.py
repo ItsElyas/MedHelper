92615e0f4cec1575b49659399c8054be66d89202
ItsElyas <elyaskh04@gmail.com>
2025-06-07 17:39:47 -0400

    Added SQL so now i can start saving data without it being local

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__) # Create a Flask application
 
-app.config['SQLA']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///medicines.db' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # This suppresses a warning
 
-
+db = SQLAlchemy(app)    #initilizes the database
 
 @app.route('/') # Defines the route for the index page
 def index():    # Function to handle requests to the index page
