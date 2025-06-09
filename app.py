68d20d084822767dbded64189f4ec95d8fd59aa4
ItsElyas <elyaskh04@gmail.com>
2025-06-08 21:35:57 -0400

    Made the SQL Table will learn how to set it up with user data tmrw

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # This suppresses a warning
 
 db = SQLAlchemy(app)    #initilizes the database
 
+class Medicine(db.Model):   #This creats a table called 'medicine
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    dosage = db.Column(db.String(50), nullable=False)
+    time = db.Column(db.String(20), nullable=False)
+    
+    def __repr__(self):
+        return f'<Medicine {self.name}>'
+    
+
 @app.route('/') # Defines the route for the index page
 def index():    # Function to handle requests to the index page
     return render_template('index.html')
