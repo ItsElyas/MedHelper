0742cc65f6768bdb228317fe221105e683761494
ItsElyas <elyaskh04@gmail.com>
2025-06-19 15:22:54 -0400

    fixed addmedicine so it hides the list when using the form and also changed the med list css

--- static/script.js
+++ static/script.js
@@ -16,42 +16,42 @@ function cancelMedicine() {
     
 }
 
-function submitMedicine() {
-    //Prevents page refreshing
-    event.preventDefault();
+// function submitMedicine() {
+//     //Prevents page refreshing
+//     event.preventDefault();
 
-    // Gets all the values of the inputs
-    const name = document.getElementById("medicineName").value;
-    const dose = document.getElementById("medicineDose").value;
-    const frequency = document.getElementById("Frequency").value;
-    const time = document.getElementById("timeToTake").value;
+//     // Gets all the values of the inputs
+//     const name = document.getElementById("medicineName").value;
+//     const dose = document.getElementById("medicineDose").value;
+//     const frequency = document.getElementById("Frequency").value;
+//     const time = document.getElementById("timeToTake").value;
 
-    // Makes sure the user fills everything out
-    if(!name || !dose || !frequency || !time) {
-        alert("Please fill out all fields.");
-        return
-    }
+//     // Makes sure the user fills everything out
+//     if(!name || !dose || !frequency || !time) {
+//         alert("Please fill out all fields.");
+//         return
+//     }
 
-    medCount++;
+//     medCount++;
     
-    const newMedDiv = document.createElement('div');
-    newMedDiv.className = "yourMedicineList";
+//     const newMedDiv = document.createElement('div');
+//     newMedDiv.className = "yourMedicineList";
 
-    newMedDiv.innerHTML = `
-        <strong>${name}</strong><br>
-        Dose: ${dose}mg<br>
-        Time: ${time}<br>`
-        ;
-           //querySelector accesses an id/class appendChild adds a new instance of a variable
-    document.querySelector(".yourMedicineList").appendChild(newMedDiv);
+//     newMedDiv.innerHTML = `
+//         <strong>${name}</strong><br>
+//         Dose: ${dose}mg<br>
+//         Time: ${time}<br>`
+//         ;
+//            //querySelector accesses an id/class appendChild adds a new instance of a variable
+//     document.querySelector(".yourMedicineList").appendChild(newMedDiv);
 
-    // Hide "No medications" message
-    document.querySelectorAll("#yourMedicine #noMedicine").forEach(el => el.style.display = "none");
+//     // Hide "No medications" message
+//     document.querySelectorAll("#yourMedicine #noMedicine").forEach(el => el.style.display = "none");
 
     
-    document.querySelector(".medicineForm").reset();    // clears form
-    document.getElementById("MedicineDiv").style.display = "none";  //hides form
-    document.getElementById("addMedicine").style.display = "inline-block";  //reshows the add medicine btn
+//     document.querySelector(".medicineForm").reset();    // clears form
+//     document.getElementById("MedicineDiv").style.display = "none";  //hides form
+//     document.getElementById("addMedicine").style.display = "inline-block";  //reshows the add medicine btn
 
-}
+// }
 
