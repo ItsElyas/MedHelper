01d58bf97ca7a2e322fdb233a85c93a5abdf625f
ItsElyas <elyaskh04@gmail.com>
2025-08-07 17:35:18 -0400

    Trying to fix final bugs but rlly stuck on them

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, jsonify # type: ignore
 from flask_sqlalchemy import SQLAlchemy # type: ignore
-from datetime import datetime
+from datetime import datetime, timedelta
 app = Flask(__name__) # Create a Flask application
 
 
@@ -24,11 +24,11 @@ class Medicine(db.Model):
 
 #INDEX ROUTE: This route is for the main page of the website. its use for it is for most of the data on the main page and 
 # shows the data for the user like totalMeds taken, all meds, and the list of when to take the meds
-
 @app.route('/', methods=['POST', 'GET']) # Defines the route for the index page
-def index():    # Function to handle requests to the index page
+def index():
     medicine_time = None    
-    current_time = datetime.now().time # This is grabbing the current time but dont know how well it works
+    time_left = None
+    current_time = datetime.now() # This is grabbing the current time but dont know how well it works   
     if request.method == 'POST':       # If the request method is post which it is since since we have the submit it grabs the data
         name = request.form['medicineName']
         dosage = request.form['medicineDose']
@@ -37,9 +37,23 @@ def index():    # Function to handle requests to the index page
         
         if not name or not dosage:
             return redirect('/')
+        
+        #Wont Work 
+        medicine_time = datetime.combine(date.today(), datetime.strptime(doseTime, '%H:%M').time())  # This is grabbing the dose time and converting it in hour : minuts
+        time_left = medicine_time - current_time
+        
+        if time_left.total_seconds() < 0:
+            missed = True
+        else:
+            missed = False
             
-        medicine_time = datetime.strptime(doseTime, '%H:%M').time() # This is grabbing the dose time and converting it in hour : minuts
-        new_medicine = Medicine(name=name, dosage=dosage, time=medicine_time, comments=notes)   # this is saving a new medicine with all the data the user added and saving it in the class and DB
+        next_med = None
+        for med in sorted_meds:
+            if med.time > current_time:
+                next_med = med
+                break
+            
+        new_medicine = Medicine(name=name, dosage=dosage, time=medicine_time, comments=notes, TimeLeft = time_left)   # this is saving a new medicine with all the data the user added and saving it in the class and DB
 
         try:
             db.session.add(new_medicine)
@@ -48,20 +62,20 @@ def index():    # Function to handle requests to the index page
         
         except:
             return 'There was an error adding your medication'
-    # This else happens when the Post is done and now the data from the post will go here  
-    else: 
+         
+    else: # This else happens when the Post is done and now the data from the post will go here 
         meds = Medicine.query.all() # grabs all the medicine in the Med Class and saves it in meds
         totalMedications = Medicine.query.count()   # This just counts the meds and uses it for percentages
-        notes = [med.comments for med in meds]
+        notes = [med.comments for med in meds]      # saving all the notes for any meds that need them
         # Need to understand this ASAP
         sorted_meds = sorted(meds, key=lambda med: med.time if med.time is not None else datetime.min.time())
         upcoming_medsToday =[]
-        return render_template('index.html', medicines=meds, orderedMeds=sorted_meds, totalMeds=totalMedications, medNotes=notes, medTime = medicine_time, currentTime = current_time, medToEdit = None)
+        return render_template('index.html', medicines=meds, orderedMeds=sorted_meds, totalMeds=totalMedications, medNotes=notes, medTime = medicine_time, currentTime = current_time, timeLeft = time_left, medToEdit = None) # Puts all the data saved on the web
 
 #DELETE ROUTE: This is for deleting the meds that are no longer being taking by the user
 @app.route('/delete/<int:id>')
 def delete(id):
-    medToDelete = Medicine.query.get_or_404(id)
+    medToDelete = Medicine.query.get_or_404(id)     #grabs the med from the class using the id or gives the user an error
     
     try:
         db.session.delete(medToDelete)
@@ -70,7 +84,8 @@ def delete(id):
     
     except:
         return 'There was a problem deleting your medications'
-    
+
+#EDIT ROUTE: Chooses the med you want to edit and changes what is saved in the database like the name time and dosage    
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
     medToChange = Medicine.query.get_or_404(id)
